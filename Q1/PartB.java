a182310b332923349ffdbe5d25419be2929bc2a3
Ahmad-0707 <ahmadjono@nycstudents.net>
2025-05-12 22:24:53 -0400

    Finialize PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,7 +1,15 @@
+//Part B
 public int dogWalkShift(int startHour, int endHour){
-  
+    int totalPay =  0;
+    for(int i = startHour;i <= endHour; i++){
+        int dogsWalked = walkDogs(hour);
 
+        int pay = dogsWalked * 5;//Base pay
 
-
-
+        if(dogsWalked == maxDogs || (hour >= 9 && hour <= 17)){
+            pay += 3;
+        }
+        totalPay += pay;
+    }
+    return totalPay;
 }
