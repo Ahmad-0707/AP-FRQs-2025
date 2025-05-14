60c999c7c79b471bc686e8071a1d0b4d5525de5d
Ahmad-0707 <ahmadjono@nycstudents.net>
2025-05-14 09:38:27 -0400

    Update PartB.java

--- Q4/PartB.java
+++ Q4/PartB.java
@@ -1,12 +1,15 @@
 public boolean clearPair(int row, int col) {
-    int targetValue = puzzle[row][col];
-    if (targetValue == 0) {
-        return false;
-    }
-    for (int i = row; i < puzzle.length; i++) {
-      for (int j = 0; j < puzzle[i].length; j++) {
-      
-      }
+    int target = puzzle[row][col];
+    for (int r = row; r < puzzle.length; r++) {
+        int startCol = (r == row) ? col + 1 : 0;
+        for (int c = startCol; c < puzzle[0].length; c++) {
+            int val = puzzle[r][c];
+            if (val != 0 && (val == target || val + target == 10)) {
+                puzzle[row][col] = 0;
+                puzzle[r][c] = 0;
+                return true;
+            }
+        }
     }
     return false;
 }
