f72c5d8098a00fa346afcb850263ff7d402f7c73
Ahmad-0707 <ahmadjono@nycstudents.net>
2025-05-13 21:43:59 -0400

    Create PartB.java

---
+++ Q4/PartB.java
@@ -0,0 +1,12 @@
+public boolean clearPair(int row, int col) {
+    int targetValue = puzzle[row][col];
+    if (targetValue == 0) {
+        return false;
+    }
+    for (int i = row; i < puzzle.length; i++) {
+      for (int j = 0; j < puzzle[i].length; j++) {
+      
+      }
+    }
+    return false;
+}
